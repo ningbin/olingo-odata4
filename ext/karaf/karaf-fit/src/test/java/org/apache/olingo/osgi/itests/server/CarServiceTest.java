584d26a906cac555ea200140f4e11526a6df8b31
Christian Amend <christian.amend@sap.com>
2016-09-09 12:56:16 +0200

    Set next 4.3.0-sap-02-SNAPSHOT version

--- ext/karaf/karaf-fit/src/test/java/org/apache/olingo/osgi/itests/server/CarServiceTest.java
+++ ext/karaf/karaf-fit/src/test/java/org/apache/olingo/osgi/itests/server/CarServiceTest.java
@@ -96,7 +96,7 @@ public class CarServiceTest extends OlingoOSGiTestSupport {
         return new Option[] {
             olingoBaseConfig(),
             features(olingoUrl, "olingo-server", "olingo-client"),
-            mavenBundle("org.apache.olingo", "odata-server-osgi-sample", "4.3.0-sap-01"),
+            mavenBundle("org.apache.olingo", "odata-server-osgi-sample", "4.3.0-sap-02-SNAPSHOT"),
             logLevel(LogLevel.INFO)
         };
     }
